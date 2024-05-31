3f5f043c58354b580e45dbea15e6df195bafb162
Habib-0007 <adebayohabib7@gmail.com>
2024-05-31 12:34:30 -0700

    Worked on the Quiz logic

--- src/components/EachQuiz.jsx
+++ src/components/EachQuiz.jsx
@@ -1,10 +1,14 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 import Toggle from "./Toggle";
+import { useState } from "react";
 
 function EachQuiz() {
   const { id } = useParams();
   const { data, isPending, error } = useFetch("/data.json");
+  const [clicked, setClicked] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   var questions = "";
   if (data && id == "html") {
     questions = data.quizzes[0].questions;
@@ -15,8 +19,6 @@ function EachQuiz() {
   } else if (data && id == "accessibility") {
     questions = data.quizzes[3].questions;
   }
-
-  var currentIndex = 0;
   var questionNum = 1;
 
   return (
@@ -36,21 +38,45 @@ function EachQuiz() {
             <Toggle />
           </div>
           <div>
-            <em className="question-no">Question 1 of {questions.length}</em>
+            <em className="question-no">
+              Question {currentIndex + 1} of {questions.length}
+            </em>
             <section className="question-box">
               <section className="question-top">
                 <strong className="question">
                   {questions[currentIndex].question}
                 </strong>
-                <progress></progress>
+                <progress min={1} value={currentIndex + 1} max={10}></progress>
               </section>
               <section className="buttons">
                 {questions[currentIndex].options.map((opt, index) => (
-                  <button className="options" key={index}>
+                  <button
+                    className={`options ${clicked === opt ? "clicked" : ""}`}
+                    key={index}
+                    onClick={(event) => {
+                      setClicked(event.target.textContent);
+                    }}
+                  >
                     {opt}
                   </button>
                 ))}
-                <input type="button" value="Submit" />
+                <input
+                  type="button"
+                  value="Submit"
+                  onClick={() => {
+                    console.log("first");
+                  }}
+                />
+                <input
+                  type="button"
+                  value="Next"
+                  className={
+                    questions.length - 1 === currentIndex ? "none" : ""
+                  }
+                  onClick={() => {
+                    setCurrentIndex(currentIndex + 1);
+                  }}
+                />
               </section>
             </section>
           </div>
